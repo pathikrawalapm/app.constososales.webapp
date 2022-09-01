3d1bff50109e99f517fcc68084b5cb87040aa571
pathikms <pathikrawal@microsoft.com>
2022-09-01 15:34:07 +0100

    n

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,12 +5,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace app.constososales.webapp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        private static string Host = "psql-contososales-uksouth-dev.postgres.database.azure.com";
+        private static string User = "psqladmin@psql-contososales-uksouth-dev";
+        private static string DBname = "salesdb";
+        private static string Password = "@mYD9@wp6zE3%rKLBJG2iSXb";
+        private static string Port = "5432";
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +26,50 @@ namespace app.constososales.webapp.Pages
 
         public void OnGet()
         {
+            string connString =
+               String.Format(
+                   "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                   Host,
+                   User,
+                   DBname,
+                   Port,
+                   Password);
 
+            using (var conn = new NpgsqlConnection(connString))
+
+            {
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DROP TABLE IF EXISTS inventory", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished dropping table (if existed)");
+
+                }
+
+                using (var command = new NpgsqlCommand("CREATE TABLE inventory(id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER)", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished creating table");
+                }
+
+                using (var command = new NpgsqlCommand("INSERT INTO inventory (name, quantity) VALUES (@n1, @q1), (@n2, @q2), (@n3, @q3)", conn))
+                {
+                    command.Parameters.AddWithValue("n1", "banana");
+                    command.Parameters.AddWithValue("q1", 150);
+                    command.Parameters.AddWithValue("n2", "orange");
+                    command.Parameters.AddWithValue("q2", 154);
+                    command.Parameters.AddWithValue("n3", "apple");
+                    command.Parameters.AddWithValue("q3", 100);
+
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
+                }
+            }
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
         }
     }
 }
